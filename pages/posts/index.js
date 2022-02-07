d5beec13ddf54d6a8fe07614ce5ede55a2073991
SergioGarciaGil <sergio.cuerda@gmail.com>
2022-02-06 22:47:53 -0500

    cliente al lado del servidor

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,10 +1,38 @@
-import Layout from "../../components/layout";
 import Title from "../../components/title";
+import Layout from "../../components/layout";
+import Link from "next/link";
+import React from "react";
 
-export default function Posts() {
+// CLIENTE SIDE RENDERIN..
+
+// export default function Posts() {
+//   const [posts, setPosts] = React.useState([]);
+
+//   React.useEffect(() => {
+//     const fetchPosts = async () => {
+//       const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+//       const newPosts = await res.json();
+//       setPosts(newPosts);
+//     };
+
+//     fetchPosts();
+//   }, []);
+
   return (
     <Layout>
-      <Title>Post Page</Title>
+      <Title>Posts Page</Title>
+      <div>
+        {posts.map((post) => {
+          return (
+            <Link href={`/posts/[id]`} as={`/posts/${post.id}`} key={post.id}>
+              <a>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+              </a>
+            </Link>
+          );
+        })}
+      </div>
     </Layout>
   );
 }
