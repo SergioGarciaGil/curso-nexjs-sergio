7fbcbb1276ce1df6e5c483e2c84bee61ef21444e
SergioGarciaGil <sergio.cuerda@gmail.com>
2022-02-08 12:16:30 -0500

    getServerSideProps

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,13 +1,61 @@
-import { useRouter } from "next/router";
 import Layout from "../../components/layout";
 import Title from "../../components/title";
 
-export default function Post() {
-  const router = useRouter();
+export default function Post({ post }) {
   return (
     <Layout>
       <Title>Post Details</Title>
-      <p>Post ID: {router.query.id}</p>
+      <div className="card1" id="grid1">
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+      </div>
+      <style jsx>
+        {`
+          #grid1 {
+            display: flex;
+            flex-wrap: wrap;
+            max-width: 600px;
+            margin-top: 2rem;
+          }
+          .card1 {
+            margin: 2rem;
+            flex-basis: 45%;
+            padding: 1.5rem;
+            text-align: left;
+            color: black;
+            text-decoration: none;
+            border: 2px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+          }
+          .card1:hover,
+          .card1:focus,
+          .card1:active {
+            color: #0070f3;
+            border-color: #0070f3;
+          }
+          .card1 h3 {
+            margin: 0 0 1rem 0;
+            font-size: 1.5rem;
+          }
+          .card1 p {
+            margin: 0;
+            font-size: 1.25rem;
+            line-height: 1.5;
+          }
+        `}
+      </style>
     </Layout>
   );
+}
+export async function getServerSideProps({ params }) {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.id}`
+  );
+  const post = await res.json();
+  return {
+    props: {
+      post,
+    },
+  };
 }
